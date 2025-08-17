b4a6dc491e727f2f2e24de8bfe05046d13c087d0
arif rakhmanov <arif.rakhmanov@gmail.com>
2025-08-17 14:53:41 +0400

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by arif rakhmanov on 8/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
