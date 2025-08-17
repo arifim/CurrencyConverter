9d90d0577508e4ef1a83e1c5935acc5ce86d62c5
arif rakhmanov <arif.rakhmanov@gmail.com>
2025-08-17 15:39:36 +0400

    Fixed the "Immutable property will not be decoded because it is declared with an initial value which cannot be overwritten" error

--- CurrencyConverter/Models/Currency.swift
+++ CurrencyConverter/Models/Currency.swift
@@ -9,10 +9,17 @@ import Foundation
 
 struct Currency: Codable, Identifiable, Equatable {
     
-    let id = UUID()
+    let id = UUID() // This property has a default value and should not be overwritten by JSON decoding
     let code: String
     let name: String
     let flag: String
+    
+    // Only include properties that should be decoded from JSON
+    enum CodingKeys: String, CodingKey {
+        case code
+        case name
+        case flag
+    }
 }
 
 struct CurrencyList: Codable {
